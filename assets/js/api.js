ecfb2b11f244990f361471c9832af4c8669da7dd
Thiago Feldner <tfeldner@hotmail.com>
2023-03-31 15:22:47 -0300

    atualizado o caminho das imagens

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,7 @@
 
 async function fetchProfileData () {
   const url = 'https://raw.githubusercontent.com/thiagofeldner/Portfolio/master/profile.json';
-  const fetching = await fetch(url)
-  return await fetching.json()
+  const response = await fetch(url)
+  const profileData = await response.json()
+  return profileData
 }
