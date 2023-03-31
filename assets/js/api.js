597938f4347b5a722784c0500a97808963495b8b
Thiago Feldner <tfeldner@hotmail.com>
2023-03-31 16:24:06 -0300

    correções dos arquivos api.js e profile.json

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData () {
-  const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
+  const url = 'https://raw.githubusercontent.com/thiagofeldner/Portfolio/master/data/profile.json';
   const response = await fetch(url)
   const profileData = await response.json()
   return profileData
